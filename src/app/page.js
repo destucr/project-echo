bab82358c3bc429ec4897b3836cd5a98bd0aca57
destucr <destucr@gmail.com>
2025-01-08 22:35:56 +0700

    update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -20,7 +20,7 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly. Bleh</li>
+          <li>Save and see your changes instantly. Bleh Bleh</li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
